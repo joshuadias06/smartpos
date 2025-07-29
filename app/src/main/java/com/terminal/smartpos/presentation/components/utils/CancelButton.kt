8aff4a4372583616188ce9a2047f11130eae4dc1
joshuadias06 <joshuadiasbf@gmail.com>
2025-07-29 18:28:03 -0300

    Create a PaymentMethodScreen

--- app/src/main/java/com/terminal/smartpos/presentation/components/utils/CancelButton.kt
+++ app/src/main/java/com/terminal/smartpos/presentation/components/utils/CancelButton.kt
@@ -1,10 +1,40 @@
 package com.terminal.smartpos.presentation.components.utils
 
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CancelButton(){
+fun CancelButton(
+    onSwitchStore: () -> Unit
+){
 
+    TextButton(
+        onClick = onSwitchStore,
+        colors = ButtonDefaults.textButtonColors(
+            contentColor = Color(0xFFEF0000)
+        )) {
+        Text("Cancelar",
+            style = MaterialTheme.typography.bodyLarge,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CancelButtonPreview(){
+    CancelButton( onSwitchStore = { TODO()})
 }
--- app/src/main/java/com/terminal/smartpos/presentation/screens/method/PaymentMethodScreen.kt
+++ app/src/main/java/com/terminal/smartpos/presentation/screens/method/PaymentMethodScreen.kt
@@ -1,7 +1,65 @@
 package com.terminal.smartpos.presentation.screens.method
 
-fun PaymentMethodScreen() {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.terminal.smartpos.presentation.components.methods.PaymentMethods
+import com.terminal.smartpos.presentation.components.utils.CancelButton
 
+@Composable
+fun PaymentMethodScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ){
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 14.dp, vertical = 12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ){
+            Spacer(modifier = modifier.padding(top = 30.dp))
+            Text(
+                text = "Formas de Pagamento",
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.SansSerif,
+                color = Color.Black,
+                maxLines = 1,
+                softWrap = false,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = modifier.padding(top = 20.dp))
+            PaymentMethods()
+            Spacer(modifier= modifier.padding(bottom = 20.dp))
+            CancelButton( onSwitchStore = {})
+        }
 
+    }
+}
 
+@Preview(showBackground = true, device = "spec:width=320dp,height=533dp,dpi=240")
+@Composable
+fun PaymentMethodScreenPreview(){
+    PaymentMethodScreen()
 }
