2981104895804e3d97477016fe4f4a3b610b5c4a
joshuadias06 <joshuadiasbf@gmail.com>
2025-07-31 20:08:45 -0300

    Update icons PaymentMethods

--- app/src/main/java/com/terminal/smartpos/presentation/components/methods/PaymentMethods.kt
+++ app/src/main/java/com/terminal/smartpos/presentation/components/methods/PaymentMethods.kt
@@ -1,10 +1,7 @@
 package com.terminal.smartpos.presentation.components.methods
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -32,7 +29,7 @@ fun PaymentMethods() {
         PaymentMethod("Débito", Icons.Default.CreditCard) { /* TODO */ },
         PaymentMethod("Crédito", Icons.Default.CreditCard) { /* TODO */ },
         PaymentMethod("PIX", Icons.Default.Pix) { /* TODO */ },
-        PaymentMethod("Voucher", Icons.Default.CreditCard) { /* TODO */ },
+        PaymentMethod("Voucher", Icons.Default.Fastfood) { /* TODO */ },
         PaymentMethod("QR Code Débito", Icons.Default.QrCode) { /* TODO */ },
         PaymentMethod("QR Code Crédito", Icons.Default.QrCode) { /* TODO */ },
     )
